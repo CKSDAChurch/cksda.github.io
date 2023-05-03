5413f092bc86c4451f74777e03ce21426027c49d
Jared Powrie <jnpowrie@southern.edu>
2023-05-03 14:15:20 -0400

    % add basic multi-language support

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,139 +27,184 @@
 
 })(jQuery);
 
-var url = window.location.href
+//// LANGUAGE SPECIFIC INFO ////
+let json;
+let lang = navigator.language;
+if (lang.startsWith("ko")) lang = "ko";
+else if (lang.startsWith("es")) lang = "es";
+else lang = "en";
+let langJson = "./assets/langStrings/" + lang + ".json";
 
-//// ADDING HEADER ////
+const getJson = async () => {
+	const response = await fetch(langJson);
+	const data = await response.json();
+	json = data;
+	return data;
+};
 
-var header = `<!-- <span class="logo icon fa-paper-plane"></span> -->
-<span class="logo icon">
-	<img src="./images/logo-light.png"/>
-</span>`
-if (url.endsWith("calendar.html") || url.endsWith("calendar")) {
-	header += `<p>
-	<a href="./index.html">Home</a> |
-	<a href="./connection.html">Connection Card</a> |
-	<a href="./college.html">College</a> |
-	<a href="./music.html">Music Ministry</a> |
-	<a href="./pathfinders.html">Pathfinders</a> |
-    <a href="https://adventistgiving.org/#/org/ANTFHH/envelope/start" target="_blank">AdventistGiving</a>
-</p>
-<h1>CKSDA Church Calendar</h1>
-<p>If you know of an event that hasn't been added to the church calendar, please contact clerk@cksda.church with the details.</p>`
-} else if (url.endsWith("college.html") || url.endsWith("college")) {
-	header += `<p>
-	<a href="./index.html">Home</a> |
-	<a href="./connection.html">Connection Card</a> |
-	<a href="./calendar.html">Calendar</a> |
-	<a href="./music.html">Music Ministry</a> |
-	<a href="./pathfinders.html">Pathfinders</a> |
-    <a href="https://adventistgiving.org/#/org/ANTFHH/envelope/start" target="_blank">AdventistGiving</a>
-	</p>
-	<h1>CKSDA Church Collegiate Ministry</h1>`
-} else if (url.endsWith("music.html") || url.endsWith("music")) {
-	header += `<p>
-		<a href="./index.html">Home</a> |
-		<a href="./connection.html">Connection Card</a> |
-		<a href="./calendar.html">Calendar</a> |
-		<a href="./college.html">College</a> |
-		<a href="./pathfinders.html">Pathfinders</a> |
-		<a href="https://adventistgiving.org/#/org/ANTFHH/envelope/start" target="_blank">AdventistGiving</a>
-	</p>
-	<h1>CKSDA Church Music Ministry</h1>`
-} else if (url.endsWith("pathfinders.html") || url.endsWith("pathfinders")) {
-	header += `<p>
-	<a href="./index.html">Home</a> |
-	<a href="./connection.html">Connection Card</a> |
-	<a href="./calendar.html">Calendar</a> |
-	<a href="./college.html">College</a> |
-	<a href="./music.html">Music Ministry</a> |
-    <a href="https://adventistgiving.org/#/org/ANTFHH/envelope/start" target="_blank">AdventistGiving</a>
-	</p>
-	<h1>Pioneers Pathfinders Club</h1>`
-} else if (url.endsWith("epoch.html") || url.endsWith("epoch")) {
-	header += `<p>
-	<a href="./index.html">Home</a> |
-	<a href="./connection.html">Connection Card</a> |
-	<a href="./calendar.html">Calendar</a> |
-	<a href="./college.html">College</a> |
-	<a href="./music.html">Music Ministry</a> |
-	<a href="./pathfinders.html">Pathfinders</a> |
-    <a href="https://adventistgiving.org/#/org/ANTFHH/envelope/start" target="_blank">AdventistGiving</a>
-	</p>
-	<h1>CKSDA Church Epoch</h1>
-	<p>Some memories from our yearly epoch</p>`
-} else {
-	header += `<p>
-	<a href="./connection.html">Connection Card</a> |
-	<a href="./calendar.html">Calendar</a> |
-	<a href="./college.html">College</a> |
-	<a href="./music.html">Music Ministry</a> |
-	<a href="./pathfinders.html">Pathfinders</a> |
-    <a href="https://adventistgiving.org/#/org/ANTFHH/envelope/start" target="_blank">AdventistGiving</a>
-</p>
-	<h1>Collegedale Korean SDA Church</h1>
-<p>To share the everlasting gospel while providing an environment were we can all grow together in the Grace of God; <br />in doing so prepare our community for the eminent return of our Lord and Savior Jesus Christ.</p>`
-}
+(async () => {
+	await getJson();
 
-document.getElementById("header").innerHTML = header
+	//// ADDING HEADER ////
+	var logo = "<img src=\"./images/logo-light.png\"/>";
+	var title = json._title;
+	var subtitle = json._subtitle;
+
+	var calendarUrl = json.menuItems.calendarURL;
+	var collegeUrl = json.menuItems.collegeURL;
+	var connectionCardUrl = json.menuItems.connectionCardURL;
+	var homeUrl = json.menuItems.homeURL;
+	var epochUrl = json.menuItems.epochURL;
+	var musicUrl = json.menuItems.musicMinistryURL;
+	var pathfindersUrl = json.menuItems.pathfindersURL;
+
+	var urlList = `<a href="` + homeUrl + `">` + json.menuItems.home + `</a> |
+<a href="`+ connectionCardUrl + `">` + json.menuItems.connectionCard + `</a> |
+<a href="`+ calendarUrl + `">` + json.menuItems.calendar + `</a> |
+<a href="`+ collegeUrl + `">` + json.menuItems.college + `</a> |
+<a href="`+ musicUrl + `">` + json.menuItems.musicMinistry + `</a> |
+<a href="`+ pathfindersUrl + `">` + json.menuItems.pathfinders + `</a> |
+<a href="https://adventistgiving.org/#/org/ANTFHH/envelope/start" target="_blank">`+ json.menuItems.adventistGiving + `</a>`
+
+	var url = window.location.href
+	//// CALENDER PAGE ////
+	if (url.endsWith(calendarUrl) || url.endsWith(calendarUrl.substring(0, (calendarUrl.length - 5)))) {
+		title = json.pageTitles.calendarTitle;
+		subtitle = json.pageTitles.calendarSubtitle;
+		urlList = urlList.replace(`<a href="` + calendarUrl + `">` + json.menuItems.calendar + `</a> |`, "");
+
+		document.getElementById("browserTitle").innerHTML = json.calendarPage.browserTitle;
+		document.getElementById("calFSMsg").innerHTML = json.calendarPage.calFSMsg;
+	}
+	//// COLLEGE PAGE ////
+	else if (url.endsWith(collegeUrl) || url.endsWith(collegeUrl.substring(0, (collegeUrl.length - 5)))) {
+		title = json.pageTitles.collegeTitle;
+		subtitle = "";
+		urlList = urlList.replace(`<a href="` + collegeUrl + `">` + json.menuItems.college + `</a> |`, "");
+
+		document.getElementById("browserTitle").innerHTML = json.collegePage.browserTitle;
+		document.getElementById("importantLinksTitle").innerHTML = json.collegePage.importantLinksTitle;
+		document.getElementById("link1").innerHTML = json.collegePage.link1;
+		document.getElementById("link2").innerHTML = json.collegePage.link2;
+		document.getElementById("eventsTitle").innerHTML = json.collegePage.eventsTitle;
+		document.getElementById("event1").innerHTML = json.collegePage.event1;
+		document.getElementById("event2").innerHTML = json.collegePage.event2;
+		document.getElementById("socialMediaTitle").innerHTML = json.collegePage.socialMediaTitle;
+	}
+	//// CONNECTION PAGE ////
+	else if (url.endsWith(connectionCardUrl) || url.endsWith(connectionCardUrl.substring(0, (connectionCardUrl.length - 5)))) {
+		document.getElementById("browserTitle").innerHTML = json.connectionPage.browserTitle;
+	}
+	//// MUSIC PAGE ////
+	else if (url.endsWith(musicUrl) || url.endsWith(musicUrl.substring(0, (musicUrl.length - 5)))) {
+		title = json.pageTitles.musicMinistryTitle;
+		subtitle = "";
+		urlList = urlList.replace(`<a href="` + musicUrl + `">` + json.menuItems.musicMinistry + `</a> |`, "");
+
+		document.getElementById("browserTitle").innerHTML = json.musicPage.browserTitle;
+		document.getElementById("socialMediaTitle").innerHTML = json.musicPage.socialMediaTitle;
+	}
+	//// PATHFINDERS PAGE ////
+	else if (url.endsWith(pathfindersUrl) || url.endsWith(pathfindersUrl.substring(0, (pathfindersUrl.length - 5)))) {
+		title = json.pageTitles.pathfindersTitle;
+		subtitle = "";
+		urlList = urlList.replace(`<a href="` + pathfindersUrl + `">` + json.menuItems.pathfinders + `</a> |`, "");
+
+		document.getElementById("browserTitle").innerHTML = json.pathfindersPage.browserTitle;
+		document.getElementById("getReadyTitle").innerHTML = json.pathfindersPage.getReadyTitle;
+		document.getElementById("whoTitle").innerHTML = json.pathfindersPage.whoTitle;
+		document.getElementById("whatTitle").innerHTML = json.pathfindersPage.whatTitle;
+		document.getElementById("registrationTitle").innerHTML = json.pathfindersPage.registrationTitle;
+		document.getElementById("openingTitle").innerHTML = json.pathfindersPage.openingTitle;
+		document.getElementById("contactUsTitle").innerHTML = json.pathfindersPage.contactUsTitle;
+		document.getElementById("socialMediaTitle").innerHTML = json.pathfindersPage.socialMediaTitle;
+	}
+	//// EPOCH PAGE ////
+	else if (url.endsWith(epochUrl) || url.endsWith(epochUrl.substring(0, (epochUrl.length - 5)))) {
+		title = json.pageTitles.epochTitle;
+		subtitle = json.pageTitles.epochSubtitle;
+
+		document.getElementById("browserTitle").innerHTML = json.epochPage.browserTitle;
+	}
+	//// INDEX PAGE ////
+	else {
+		urlList = urlList.replace(`<a href="` + homeUrl + `">` + json.menuItems.home + `</a> |`, "");
+
+		document.getElementById("browserTitle").innerHTML = json.indexPage.browserTitle;
+		document.getElementById("missionParagraph").innerHTML = json.indexPage.missionParagraph;
+		document.getElementById("titheTitle").innerHTML = json.indexPage.titheTitle;
+		document.getElementById("titheParagraph1").innerHTML = json.indexPage.titheParagraph1;
+		document.getElementById("titheParagraph2").innerHTML = json.indexPage.titheParagraph2;
+	}
+
+	var header = `<span class="logo icon">` + logo + `</span>
+	<p>`+ urlList + `</p>
+	<h1>`+ title + `</h1>
+	<p>` + subtitle + `</p>`;
+
+	document.getElementById("header").innerHTML = header;
 
 
+	//// ADDING FOOTER ////
+	var pastorsTitle = json.footer.pastorsTitle;
+	var headPastor = json.footer.headPastor;
+	var headPastorTitle = json.footer.headPastorTitle;
+	var assistantPastor = json.footer.assistantPastor;
+	var assistantPastorTitle = json.footer.assistantPastorTitle;
+	var worshipServicesTitle = json.footer.worshipServicesTitle;
+	var korean = json.footer.korean;
+	var english = json.footer.english;
+	var ssTitle = json.footer.ssTitle;
+	var wsTitle = json.footer.wsTitle;
+	var enSStime = json.footer.enSStime;
+	var enWStime = json.footer.enWStime;
+	var koSStime = json.footer.koSStime;
+	var koWStime = json.footer.koWStime;
+	var mailingAddressTitle = json.footer.mailingAddressTitle;
+	var mailingAddress = json.footer.mailingAddress;
+	var phoneTitle = json.footer.phoneTitle;
+	var phone = json.footer.phone;
+	var websiteTitle = json.footer.websiteTitle;
+	var copyright = json.footer.copyright;
+	var advWebsiteTitle = json.footer.advWebsiteTitle;
 
-//// ADDING FOOTER ////
-
-var footer = `<div class="container medium">
-	<header class="major last">
-		<h2>Pastor</h2>
-	</header>
-
-	<p><strong>Head Pastor</strong>:<br />
-		Kangwon Yang</p>
-	<p><strong>Assistant Pastor</strong>:<br />
-		Tony Dennis</p>
-
-	<header class="major last">
-		<h2>WORSHIP SERVICES</h2>
-	</header>
+	var footer = `<div class="container medium">
+	<header class="major last"><h2>`+ pastorsTitle + `</h2></header>
+	
+	<p><strong>`+ headPastorTitle + `</strong>:<br />` + headPastor + `</p>
+	<p><strong>`+ assistantPastorTitle + `</strong>:<br />` + assistantPastor + `</p>
+	
+	<header class="major last"><h2>`+ worshipServicesTitle + `</h2></header>
 	<div class="WorshipServices">
 		<div class="WorshipChild">
-			<h4>English</h4>
-			<p><strong>Sabbath School</strong><br />
-				10:00AM - 11:15AM<br />
-
-				<strong>Worship Service</strong><br />
-				11:30AM - 1:00PM
-			</p>
+			<h4>`+ korean + `</h4>
+			<p><strong>`+ ssTitle + `</strong><br />` + koSStime + `<br />
+			<strong>`+ wsTitle + `</strong><br />` + koWStime + `</p>
 		</div>
 		<div class="WorshipChild">
-			<h4>Korean</h4>
-			<p><strong>Sabbath School</strong><br />
-				09:30AM - 10:30AM<br />
-
-				<strong>Worship Service</strong><br />
-				10:30AM - 11:15PM
-			</p>
+			<h4>`+ english + `</h4>
+			<p><strong>`+ ssTitle + `</strong><br />` + enSStime + `<br />
+			<strong>`+ wsTitle + `</strong><br />` + enWStime + `</p>
 		</div>
 	</div>
 
-	<header class="major last">
-		<h2>MAILING ADDRESS</h2>
-	</header>
-	<p>4717 Ooltewah Ringgold Rd<br />
-		Ooltewah, TN 37363<br />
-	<strong>Phone</strong>: (423) 453-3004<br />
-	<strong>Website</strong>: <a href="https://cksda.church">cksda.church</a></p>
-
+	<header class="major last"><h2>`+ mailingAddressTitle + `</h2></header>
+	<p>`+ mailingAddress + `<br />
+	<strong>`+ phoneTitle + `</strong>: ` + phone + `<br />
+	<strong>`+ websiteTitle + `</strong>: <a href="https://cksda.church">cksda.church</a></p>
+	
 	<ul class="icons">
-		<li><a href="https://youtube.com/@CKSDAChurch" class="icon brands fa-youtube" target="_blank"><span class="label">YouTube</span></a></li>
-		<li><a href="https://instagram.com/CKSDAChurch" class="icon brands fa-instagram" target="_blank"><span class="label">Instagram</span></a></li>
-		<li><a href="https://twitter.com/CKSDAChurch" class="icon brands fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
-		<li><a href="https://fb.com/CKSDAChurch" class="icon brands fa-facebook-f" target="_blank"><span class="label">Facebook</span></a></li>
+	<li><a href="https://youtube.com/@CKSDAChurch" class="icon brands fa-youtube" target="_blank"><span class="label">YouTube</span></a></li>
+	<li><a href="https://instagram.com/CKSDAChurch" class="icon brands fa-instagram" target="_blank"><span class="label">Instagram</span></a></li>
+	<li><a href="https://twitter.com/CKSDAChurch" class="icon brands fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
+	<li><a href="https://fb.com/CKSDAChurch" class="icon brands fa-facebook-f" target="_blank"><span class="label">Facebook</span></a></li>
 	</ul>
-
+	
 	<ul class="copyright">
-		<li>&copy; Collegedale Korean Seventh-Day Adventist Church</li>
-		<li><a href="http://adventist.org">Seventh-Day Adventist Church Official Website</a></li>
+	<li>`+ copyright + `</li>
+	<li><a href="http://adventist.org">`+ advWebsiteTitle + `</a></li>
 	</ul>
-</div>`
+	</div>`;
 
-document.getElementById("footer").innerHTML = footer
+	document.getElementById("footer").innerHTML = footer;
+})();
--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -21,81 +21,31 @@ if (daylightSaving) {
 
 var day = new Date().getUTCDay();
 
-if ((hour >= 19 && hour <= 20 && day == 5) || (hour >= 9 && hour <= 12 && day == 6)) { // CKSDA Church hours
+// CKSDA Church hours (EM)
+if (lang == "en" && (hour >= 11 && hour < 13 && day == 6)) {
     document.getElementById("youtubeLive").innerHTML = `<h3>Watch CKSDA Church</h3>
         <iframe style="width: 100%; height: 22em;" title="CKSDA Church Livestream" frameborder="0" allowfullscreen
         src="https://www.youtube.com/embed/live_stream?channel=UCwWsr1Z3S9SY-DfkIwUMSYQ"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>`;
-// } else { // CKSDA RSVP
-    // document.getElementById("youtubeLive").innerHTML = `<h3>RSVP for this week's service</h3>
-    // <p>Signup open at 11am each Thursday, so be sure to make your reservation early!<br />
-    //     Please remember to only RSVP for yourself, your siblings or immediate family.</p>
-    // <ul class="actions special">
-    //     <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSfncXJnaXpuyveEdghfwcwDFSAtEaNN7lES-4lFiRy8O33hEQ/viewform" class="button" target="_blank">RSVP</a></li>
-    // </ul>`;
-// }
-} else {
+}
+// CKSDA Church hours (KM)
+else if (lang == "ko" && ((hour >= 19 && hour <= 20 && day == 5) || (hour >= 9 && hour <= 11 && day == 6))) {
+    document.getElementById("youtubeLive").innerHTML = `<h3>Watch CKSDA Church</h3>
+        <iframe style="width: 100%; height: 22em;" title="CKSDA Church Livestream" frameborder="0" allowfullscreen
+        src="https://www.youtube.com/embed/live_stream?channel=UCwWsr1Z3S9SY-DfkIwUMSYQ"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>`;
+}
+// Last Week (KM)
+else if (lang == "ko") {
+    document.getElementById("youtubeLive").innerHTML = `<h3>Watch CKSDA Church</h3>
+        <iframe style="width: 100%; height: 22em;" title="YouTube video player" allowfullscreen frameborder="0"
+        src="https://www.youtube.com/embed/videoseries?index=0&list=PLIkL0-bPEL8qxyr_fpD0-8ke5zosKG-cF"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>`;
+} 
+// Last Week (EM)
+else {
     document.getElementById("youtubeLive").innerHTML = `<h3>Watch Previous Service</h3>
         <iframe style="width: 100%; height: 22em;" title="YouTube video player" allowfullscreen frameborder="0"
         src="https://www.youtube.com/embed/videoseries?index=0&list=PLIkL0-bPEL8qQUoX4JIONp-RCjgwKQFgx"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>`;
-}
-
-
-//// *********** Show the last n videos *********** ////
-// 1. The <iframe> (and video player) will replace this <div> tag.
-// <div id="player"></div>
-
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// 3. This function creates an <iframe> (and YouTube player) after the API code downloads.
-var player, player2, player3;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        videoId: 'wyswv1J8kAM',
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-    player2 = new YT.Player('player2', {
-        videoId: 'QffWImBqfuc',
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-    player3 = new YT.Player('player3', {
-        videoId: 'LN-4AfzdCNg',
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-}
-
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-    // event.target.playVideo();
-}
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-    }
-}
-function stopVideo() {
-    player.stopVideo();
-    player2.stopVideo();
-    player3.stopVideo();
 }
